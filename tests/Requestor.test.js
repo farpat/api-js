d4c994741890a804846c33530abde4ecde9d36cb
Patrick FARRUGIA <farrugiapatrick@free.fr>
2019-09-25 21:31:55 +0200

    Update tests

--- tests/Requestor.test.js
+++ tests/Requestor.test.js
@@ -1,31 +1,24 @@
-import 'whatwg-fetch';
-import fs from "fs";
-import path from "path";
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 import Requestor from "../index";
 import Http from "../src/Http";
+import 'whatwg-fetch';
 
 console.error = function () { }; //to hide fail in code
 
-jest.dontMock('fs');
-
 describe('test', function () {
-  beforeEach(function () {
-    document.documentElement.innerHTML = html.toString();
-  });
-
-  afterEach(function () {
-    //restore the origin func after test
-    jest.resetModules();
-  });
-
   it('get newRequest()', function () {
     expect(Requestor.newRequest()).toBeInstanceOf(Http);
   })
 
   it('get token', function () {
+    document.head.innerHTML = `<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="token">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>`;
     expect(Requestor.getCsrfToken()).toBe('token');
   });
 
