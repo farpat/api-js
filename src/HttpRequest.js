9b3573d9ea2a6d2d1cc42b6ef633d9ac6d2fce86
Patrick FARRUGIA <p.farrugia@outlook.fr>
2020-07-24 10:08:05 +0200

    Update to  version

--- src/HttpRequest.js
+++ src/HttpRequest.js
@@ -1,3 +1,58 @@
+/**
+ *
+ * @param {String} endPoint
+ * @param {Object} data
+ * @param {Object} headers
+ * @param {Object} config
+ * @returns {Promise<Any>}
+ */
+const fetch = async function (endPoint, data, headers, config) {
+  config.credentials = 'same-origin'
+  config.headers = {
+    ...headers,
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type'    : 'application/json',
+    'Accept'          : 'application/json'
+  }
+
+  const token = HttpRequest.getCsrfToken()
+  if (token) {
+    config.headers['X-CSRF-TOKEN'] = token
+  }
+
+  if (JSON.stringify(data) !== '{}') {
+    if (config.method === 'GET' || config.method === 'DELETE') {
+      endPoint += '?' + buildQueryString(data)
+    } else {
+      config.body = JSON.stringify(data)
+    }
+  }
+
+  const url = this.baseUrl + (endPoint.slice(0, 1) === '/' ? endPoint.slice(1) : endPoint)
+  const response = await window.fetch(url, config)
+
+  if (response.ok) {
+    return response.json()
+  } else {
+    throw response.json()
+  }
+}
+
+/**
+ *
+ * @param {Object} data
+ * @returns {string}
+ */
+const buildQueryString = function (data) {
+  const searchParameters = new URLSearchParams()
+
+  Object.keys(data).forEach(function (parameterName) {
+    searchParameters.append(parameterName, data[parameterName])
+  })
+
+  return searchParameters.toString()
+}
+
 export default class HttpRequest {
   constructor (baseUrl) {
     if (baseUrl !== '') {
@@ -23,53 +78,6 @@ export default class HttpRequest {
     return HttpRequest.csrfToken
   }
 
-  async fetch (endPoint, data, headers, config) {
-    config.credentials = 'same-origin'
-    config.headers = {
-      ...headers,
-      'X-Requested-With': 'XMLHttpRequest',
-      'Content-Type'    : 'application/json',
-      'Accept'          : 'application/json'
-    }
-
-    const token = HttpRequest.getCsrfToken()
-    if (token) {
-      config.headers['X-CSRF-TOKEN'] = token
-    }
-
-    if (JSON.stringify(data) !== '{}') {
-      if (config.method === 'GET' || config.method === 'DELETE') {
-        endPoint += '?' + this.buildQueryString(data)
-      } else {
-        config.body = JSON.stringify(data)
-      }
-    }
-
-    const url = this.baseUrl + (endPoint.slice(0, 1) === '/' ? endPoint.slice(1) : endPoint)
-    const response = await window.fetch(url, config)
-
-    if (response.ok) {
-      return response.json()
-    } else {
-      throw response.json()
-    }
-  }
-
-  /**
-   *
-   * @param {Object} data
-   * @returns {string}
-   */
-  buildQueryString (data) {
-    const searchParameters = new URLSearchParams()
-
-    Object.keys(data).forEach(function (parameterName) {
-      searchParameters.append(parameterName, data[parameterName])
-    })
-
-    return searchParameters.toString()
-  }
-
   /**
    *
    * @param {String} endPoint
@@ -80,7 +88,7 @@ export default class HttpRequest {
    */
   post (endPoint, data = {}, headers = {}, config = {}) {
     config.method = 'POST'
-    return this.fetch(endPoint, data, headers, config)
+    return fetch.call(this, endPoint, data, headers, config)
   }
 
   /**
@@ -93,7 +101,7 @@ export default class HttpRequest {
    */
   patch (endPoint, data = {}, headers = {}, config = {}) {
     config.method = 'PATCH'
-    return this.fetch(endPoint, data, headers, config)
+    return fetch.call(this, endPoint, data, headers, config)
   }
 
   /**
@@ -106,7 +114,7 @@ export default class HttpRequest {
    */
   put (endPoint, data = {}, headers = {}, config = {}) {
     config.method = 'PUT'
-    return this.fetch(endPoint, data, headers, config)
+    return fetch.call(this, endPoint, data, headers, config)
   }
 
   /**
@@ -119,7 +127,7 @@ export default class HttpRequest {
    */
   get (endPoint, data = {}, headers = {}, config = {}) {
     config.method = 'GET'
-    return this.fetch(endPoint, data, headers, config)
+    return fetch.call(this, endPoint, data, headers, config)
   }
 
   /**
@@ -132,6 +140,6 @@ export default class HttpRequest {
    */
   delete (endPoint, data = {}, headers = {}, config = {}) {
     config.method = 'DELETE'
-    return this.fetch(endPoint, data, headers, config)
+    return fetch.call(this, endPoint, data, headers, config)
   }
 }
